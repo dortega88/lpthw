90afb733396616fd088f17efab61a5c69f91c019
Daniel Ortega <dortega88@gmail.com>
2018-01-21 22:00:59 -0600

    commit

--- ex26.py
+++ ex26.py
@@ -79,25 +79,22 @@ dogs = 15
 
 if people < cats:
     print("Too many cats! The world is doomed!")
-
-if people < cats:
+else:
     print("Not many cats! The world is saved!")
 
 if people < dogs:
     print("The world is drooled on!")
-
-if people > dogs:
+else:
     print("The world is dry!")
 
-
 dogs += 5
+#print(dogs)
 
 if people >= dogs:
     print("People are greater than or equal to dogs.")
 
-if people <= dogs:
+elif people <= dogs:
     print("People are less than or equal to dogs.")
 
-
-if people == dogs:
+elif people == dogs:
     print("People are dogs.")
